f3260018966026fdbbd2b2ee9d2acfc143bb43a2
cocodro <combswc@gmail.com>
2015-02-19 22:19:48 -0500

    Finished wiki crud basic commands

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -11,6 +11,48 @@ class WikisController < ApplicationController
     @wiki = Wiki.new
   end
 
+  def create
+    @wiki = Wiki.new(wiki_params)
+    if @wiki.save
+      flash[:notice] = "Congrats, you've created a new wiki!"
+      redirect_to @wiki
+    else
+      flash[:error] = "Uh oh! There was an error creating your wiki.  Please try again."
+      render :new
+    end
+  end
+
   def edit
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    if @wiki.update_attributes(wiki_params)
+      flash[:notice] = "Wiki was updated!"
+      redirect_to @wiki
+    else
+      flash[:error] = "Uh oh! There was an error updating your wiki.  Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @wiki = Wiki.find(params[:id])
+    title = @wiki.title
+
+    if @wiki.destroy
+      flash[:notice] = "\"#{title}\" was deleted successfully."
+      redirect_to wikis_path
+    else
+      flash[:error] = "There was an error deleting this wiki."
+      render :show
+    end
+  end
+  
+  private
+
+  def wiki_params
+    params.require(:wiki).permit(:title, :body)
   end
 end
